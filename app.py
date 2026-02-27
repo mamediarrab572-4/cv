8dbaab88ef95f130994f6c1ac618918ad3949ea4
mamediarrab572-4 <mamediarrab572@gmail.com>
2026-02-27 14:13:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ st.sidebar.write("mail:mamediarrab572@gmail.com")
 st.sidebar.write("Adresse:Dakar/Senegal")
 #----Contenu principal----
 st.title("Curriculum Vitae")
+if menu==="Profil":
 st.header("Profil")
 st.write("Etudiante en 2 année de BTS Géomatique,passionnée par les systémes d'information géographique,la cartographie et la télédetection")
+elif menu =="DIPLOMES"
 st.header("DIPLOMES")
 st.write("BTS en géomatique en cours")
 st.write(" licence en géographie")
 st.write("BAC L2")
 st.header ("Technicien Superieur en Géomatique")
-st.subheader("Competences")
+elif menu =="  Compétences")
+st.header("Competences")
 st.Wwrite("maitrise des logiciels:QGIS,Arcgis, Google Earth Pro,Pix4D,Erdas,Agisoft,autocad")
 st.write("Conception de carte")
 st.write("Géoréférencement d'image")
