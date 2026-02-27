e50904b5b0c22dc4d57cd304103b88b3afc3b9bc
mamediarrab572-4 <mamediarrab572@gmail.com>
2026-02-27 13:33:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
 import streamlit as st
- st.title("mise en production")
+writh st.sidebar:
+st.write ("MAME DIARRA BA")
+st.write("mail:mamediarrab572@gmail.com")
+st.write("Adresse:Dakar/Senegal")
+st.title("Curriculum Vitae")
+st.header("Profil")
+st.write("Etudiante en 2 année de BTS Géomatique,passionnée par les systémes d'information géographique,la cartographie et la télédetection")
+st.header(" ("DIPLOMES")
+st.write("BTS en géomatique en cours")
+st.write(" licence en géographie")
+st.write("BAC L2")
+st.header ("Technicien Superieur en Géomatique")
+st.subheader("Competences")
+st.Wwrite("maitrise des logiciels:QGIS,Arcgis, Google Earth Pro,Pix4D,Erdas,Agisoft,autocad")
+st.write("Conception de carte")
+st.write("Géoréférencement d'image")
+st.write("Numerisation")
+st.write("Conception de base de données spatiales")
+st.write("Maitrise de Pack Office ("EXCEL,POWER POINT")
+st.write("Conception de plan sur autocad")
+
+
