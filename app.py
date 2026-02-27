67b8c19bf8a20eec6721f068c229ae05f6b3bcfd
mamediarrab572-4 <mamediarrab572@gmail.com>
2026-02-27 16:02:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,28 +3,28 @@ with st.sidebar:
   st.header("MAME DIARRA BA")
   st.write("Technicienne Superieur en géomatique")
   st.markdown("""
--Adresse:Dakar/Sénegal
--Mail:mamediarrab572@gmail.com""")
+- Adresse:Dakar/Sénegal
+- Mail:mamediarrab572@gmail.com""")
 st.subheader("Profil")
 st.write("""Etudiante en 2 année de BTS Géomatique,passionnée par les systémes d'information géographique,la cartographie et la télédetection
 Sérieuse,motivée;prete à éffectuer un stage en géomatique""")
 st.subheader("DIPLOMES")
 st.markdown("""
--BTS en géomatique en cours
+- BTS en géomatique en cours
 - licence en géographie
--BAC L2""")
+- BAC L2""")
 st.subheader(" Compétences")
 st.markdown("""
--maitrise des logiciels:QGIS,Arcgis, Google Earth Pro,Pix4D,Erdas,Agisoft,autocad
--Conception de carte
--Géoréférencement des cartes
--Numerisation
--Conception de base de données spatiales
--Conception de plan autocad
--Maitrise de Pack Office:EXCEL,POWER POINT""")
+- maitrise des logiciels:QGIS,Arcgis, Google Earth Pro,Pix4D,Erdas,Agisoft,autocad
+- Conception de carte
+- Géoréférencement des cartes
+- Numerisation
+- Conception de base de données spatiales
+- Conception de plan autocad
+-  Maitrise de Pack Office:EXCEL,POWER POINT""")
 st.subheader("Project Académique")
 st.markdown("""
--Projection cartographique:Collecte de données de commerce et les écoles de formation professionnel
--SIG:Création de base de données,numerisation,géoréférencement des cartes
--Protogrammetrie:Ttraitement de données sur Agisoft et PIX4D mapper""")
+- Projection cartographique:Collecte de données de commerce et les écoles de formation professionnel
+- SIG:Création de base de données,numerisation,géoréférencement des cartes
+- Protogrammetrie:Ttraitement de données sur Agisoft et PIX4D mapper""")
 
